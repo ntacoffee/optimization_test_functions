29596a661ef748fd10fb05b5247e6fe143a00b70
nTaco <67748744+ntacoffee@users.noreply.github.com>
2021-06-26 01:25:46 +0900

    add plotly and assert

--- egg_holder.py
+++ egg_holder.py
@@ -5,6 +5,7 @@
 from typing import Union
 
 import numpy as np
+import plotly.graph_objects as go
 from overrides import overrides
 
 from test_function import TestFunction
@@ -29,3 +30,26 @@ if __name__ == "__main__":
     value = egg_holder(np.array([512, 404.2319]))
     assert value > -959.6407 - 0.01
     assert value < -959.6407 + 0.01
+
+    value = egg_holder(np.array([[512, 404.2319], [512, 404.2319]]))
+    assert value[0] > -959.6407 - 0.01
+    assert value[0] < -959.6407 + 0.01
+    assert value[1] > -959.6407 - 0.01
+    assert value[1] < -959.6407 + 0.01
+
+    n_grid = 500
+    x = np.linspace(-500, 500, n_grid)
+    y = np.linspace(-500, 500, n_grid)
+    xx, yy = np.meshgrid(x, y)
+    z = egg_holder(
+        np.concatenate([np.reshape(xx, (-1, 1)), np.reshape(yy, (-1, 1))], axis=1)
+    ).reshape((n_grid, n_grid))
+
+    fig = go.Figure(data=[go.Surface(z=z, x=x, y=y)])
+    fig.update_layout(
+        scene=go.layout.Scene(
+            aspectmode="manual",
+            aspectratio=go.layout.scene.Aspectratio(x=1, y=1, z=0.5),
+        )
+    )
+    fig.show()
