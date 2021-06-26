07e8d704f81b4623ce4575572008a41faba1ac67
nTaco <67748744+ntacoffee@users.noreply.github.com>
2021-06-26 15:17:48 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup
+
+setup(
+    name="opttfunc",
+    version="0.0.0",
+    description="",
+    author="nTaco",
+    url="",
+    install_requires=open("requirements.txt").read().splitlines(),
+)
